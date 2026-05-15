54adaee6fa6f3550e8708b12e8e9bfa3c10491d6
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-05-15 03:29:30 +0300

    added custom list view item to home & good start to the day.

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 224.0.h,
+      width: 150.0.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+        image: const DecorationImage(
+          image: NetworkImage(
+            'https://upload.wikimedia.org/wikipedia/ar/thumb/0/00/%D8%BA%D9%84%D8%A7%D9%81_%D8%B1%D9%88%D8%A7%D9%8A%D8%A9_%D8%A3%D9%85%D8%A7%D8%B1%D9%8A%D8%AA%D8%A7.jpeg/250px-%D8%BA%D9%84%D8%A7%D9%81_%D8%B1%D9%88%D8%A7%D9%8A%D8%A9_%D8%A3%D9%85%D8%A7%D8%B1%D9%8A%D8%AA%D8%A7.jpeg'
+          ),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
