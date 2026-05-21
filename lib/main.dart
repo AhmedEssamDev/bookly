436cf3d511fec15aa1fe68333d1026db652de4d4
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-05-21 05:41:44 +0300

    setup service locator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:bookly/core/dependency%20injection/service_locator.dart';
 import 'package:bookly/core/router/app_router.dart';
 import 'package:bookly/core/utils/app_colors.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -13,17 +19,27 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375.0, 812.0),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp.router(
-        routerConfig: AppRouter.appRouter,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: AppColors.kPrimaryColor,
-          textTheme: GoogleFonts.montserratTextTheme(
-            ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(375.0, 812.0),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: MaterialApp.router(
+          routerConfig: AppRouter.appRouter,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: AppColors.kPrimaryColor,
+            textTheme: GoogleFonts.montserratTextTheme(
+              ThemeData.dark().textTheme,
+            ),
           ),
         ),
       ),
