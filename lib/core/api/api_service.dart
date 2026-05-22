bdc23a0c5fda9454bfc6daca9ae23d84a0d327cb
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-05-22 08:36:14 +0300

    display feature data part 1& we also fix the url problem.

--- lib/core/api/api_service.dart
+++ lib/core/api/api_service.dart
@@ -7,7 +7,7 @@ class ApiService {
   ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await _dio.get('$EndPoints.baseURL');
+    var response = await _dio.get('${EndPoints.baseURL}$endPoint');
     return response.data;
   }
 
--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,11 +4,3 @@ abstract class EndPoints {
   static const String featured = 'volumes?q=subject:programming&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc&Filtering=free-ebooks';
 }
 
-/*
-
-
-https://www.googleapis.com/books/v1/volumes?q=flutter&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc
-
-
-https://www.googleapis.com/books/v1/volumes?q=programming&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc
- */
