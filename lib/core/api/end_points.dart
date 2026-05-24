dd5d0b6c2949bbfc96cacd92267895a35386f729
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-05-24 07:29:01 +0300

    fetch similar books.

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,4 +4,6 @@ abstract class EndPoints {
       'volumes?q=subject:strategy&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc&Filtering=free-ebooks';
   static const String newest =
       'volumes?q=subject:egypt&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc&Filtering=free-ebooks&sorting=newest';
+  static String similar({required String category}) =>
+      'volumes?q=subject:$category&key=AIzaSyCVuDAEFOYaQkT4VXbQp5iw5HL0yUev_kc&Filtering=free-ebooks';
 }
