d30851609685192734812b8d3bfcea85c67a3815
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-05-28 14:42:59 +0300

    adding navigation in similar books list view to the book details view.

--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,8 +1,10 @@
+import 'package:bookly/core/router/app_router_keys.dart';
 import 'package:bookly/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
@@ -21,8 +23,16 @@ class SimilarBooksListView extends StatelessWidget {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-                  child: CustomBookImage(
-                    imageURL: state.books[index].volumeInfo.imageLinks?.thumbnail ?? '',
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(
+                        AppRouterKeys.bookDetails,
+                        extra: state.books[index],
+                      );
+                    },
+                    child: CustomBookImage(
+                      imageURL: state.books[index].volumeInfo.imageLinks?.thumbnail ?? '',
+                    ),
                   ),
                 );
               },
